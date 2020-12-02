80354b48a93df537f77da126f379a1382edd6818
madhusud <madhusud@synopsys.com>
2020-12-02 23:04:28 +0530

    Adding resource leak

--- doc-examples-java/src1/DocExamples.java
+++ doc-examples-java/src1/DocExamples.java
@@ -91,6 +91,19 @@ RESOURCE_LEAK
 	}
     }
 
+	public void ResourceLeak1()
+    {
+	try
+	{
+		FileInputStream fis = new FileInputStream("foo");  
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		
+	}
+	catch(Exception e)
+	{
+		// ignore exception
+	}
+    }
 
 /*===========================
 REVERSE_INULL
