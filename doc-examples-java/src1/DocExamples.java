db8655c0624f34716692d4347a99dc42ddf22fd7
madhusud <madhusud@synopsys.com>
2020-12-04 16:02:36 +0530

    Remove a resource leak

--- doc-examples-java/src1/DocExamples.java
+++ doc-examples-java/src1/DocExamples.java
@@ -91,23 +91,7 @@ RESOURCE_LEAK
 	}
     }
 	
-	public void ResourceLeak2()
-    {
-	try
-	{
-		FileInputStream fis = new FileInputStream("foobar");  
-		BufferedInputStream bis = new BufferedInputStream(fis);
-		
-	}
-	catch(Exception e)
-	{
-		// ignore exception
-	}
-    }
 	
-	
-	
-
 /*===========================
 REVERSE_INULL
 ===========================*/
