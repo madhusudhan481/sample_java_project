fad4a4f5bbcf27518d524536e46badbf61141b42
madhusud <madhusud@synopsys.com>
2020-12-08 15:38:11 +0530

    Adding a New leak

--- doc-examples-java/src1/DocExamples.java
+++ doc-examples-java/src1/DocExamples.java
@@ -108,6 +108,21 @@ RESOURCE_LEAK
 	}
     }
 	
+	public void ResourceLeak2()
+    {
+	try
+	{
+		FileInputStream fis = new FileInputStream("foobar");  
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		
+		
+	}
+	catch(Exception e)
+	{
+		// ignore exception
+	}
+    }
+	
 /*===========================
 REVERSE_INULL
 ===========================*/
