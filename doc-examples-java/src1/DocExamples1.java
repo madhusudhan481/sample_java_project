16f2f782c89e6f79b6a3fa33967f875a9649eb80
madhusud <madhusud@synopsys.com>
2020-12-09 18:46:14 +0530

    Adding issues in existing file 1

--- doc-examples-java/src1/DocExamples1.java
+++ doc-examples-java/src1/DocExamples1.java
@@ -122,6 +122,22 @@ RESOURCE_LEAK
 		// ignore exception
 	}
     }
+	
+	public void ResourceLeak3()
+    {
+	try
+	{
+		FileInputStream fis = new FileInputStream("foooobar1");  
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		
+		
+	}
+	catch(Exception e)
+	{
+		// ignore exception
+	}
+    }
+	
 /*===========================
 REVERSE_INULL
 ===========================*/
