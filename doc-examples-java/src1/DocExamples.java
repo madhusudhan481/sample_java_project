c8815cb695009fbdb8c68d2f1364dbbed3ee4734
madhusud <madhusud@synopsys.com>
2020-12-17 17:26:46 +0530

    Adding a r leak

--- doc-examples-java/src1/DocExamples.java
+++ doc-examples-java/src1/DocExamples.java
@@ -107,6 +107,20 @@ RESOURCE_LEAK
 	}
     }
 	
+	public void ResourceLeak2()
+    {
+	try
+	{
+		FileInputStream fis = new FileInputStream("foobar");  
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		
+	}
+	catch(Exception e)
+	{
+		// ignore exception
+	}
+    }
+	
 /*===========================
 REVERSE_INULL
 ===========================*/
